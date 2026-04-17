1bd1a2becd050061b5230d98a665137d96406c49
CiccioTecchio <ciccio@FVICIDOMINW.italy.itroot.adnet>
2026-04-17 16:49:20 +0200

    Initial commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,22 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { loginReducer } from './reducers/login.reducers';
+import { coinReducer } from './reducers/coin.reducers';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideStore({ user: loginReducer, coinCollection: coinReducer }),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: !isDevMode(),
+      autoPause: true,
+      trace: isDevMode(),
+      connectInZone: true,
+    }),
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LoginPage } from './components/login-page/login-page';
+import { HomePage } from './components/home-page/home-page';
+import { authGuard } from './guard/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  /*{
+    path: '',
+    component: LoginPage,
+  },*/
+  {
+    path: '',
+    component: HomePage,
+    //canActivate: [authGuard],
+  },
+];

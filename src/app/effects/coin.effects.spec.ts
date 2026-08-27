c155396093eb6cffdd8c20c7e1bbed30806aa671
VadaLinux <gabriele.vadala@eng.it>
2026-08-27 13:40:12 +0200

    test: cover CoinEffects cancellation and filtering
    Closes #1

--- src/app/effects/coin.effects.spec.ts
+++ src/app/effects/coin.effects.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { Actions } from '@ngrx/effects';
+import { Action } from '@ngrx/store';
 import { Subject, of, throwError } from 'rxjs';
 import { getCoinsCollection, getCoinsCollectionFailure, getCoinsCollectionSuccess } from '../actions/coin.actions';
 import { Coin } from '../models/Coin';
@@ -7,7 +8,7 @@ import { CoinService } from '../services/coin-service';
 import { CoinEffects } from './coin.effects';
 
 describe('CoinEffects', () => {
-  let actions$: Subject<ReturnType<typeof getCoinsCollection>>;
+  let actions$: Subject<Action>;
   let service: { getCoinCollection: jest.Mock };
   let effects: CoinEffects;
 
@@ -54,5 +55,38 @@ describe('CoinEffects', () => {
       done();
     });
     actions$.next(getCoinsCollection());
+  });
+
+  it('does not emit or call the service for unrelated actions', () => {
+    const emitted: Action[] = [];
+    const subscription = effects.loadCoinsCollection$.subscribe((action) => emitted.push(action));
+
+    actions$.next({ type: '[Test] Unrelated action' });
+
+    expect(service.getCoinCollection).not.toHaveBeenCalled();
+    expect(emitted).toEqual([]);
+    subscription.unsubscribe();
+  });
+
+  it('cancels an in-flight request when a newer collection request arrives', () => {
+    const firstRequest$ = new Subject<Coin[]>();
+    const secondRequest$ = new Subject<Coin[]>();
+    const firstCoins = [new Coin('Old result', '', 'Italy', 2002, 1, 1)];
+    const latestCoins = [new Coin('Latest result', '', 'France', 2003, 2, 2)];
+    const emitted: Action[] = [];
+
+    service.getCoinCollection
+      .mockReturnValueOnce(firstRequest$)
+      .mockReturnValueOnce(secondRequest$);
+    const subscription = effects.loadCoinsCollection$.subscribe((action) => emitted.push(action));
+
+    actions$.next(getCoinsCollection());
+    actions$.next(getCoinsCollection());
+    firstRequest$.next(firstCoins);
+    secondRequest$.next(latestCoins);
+
+    expect(service.getCoinCollection).toHaveBeenCalledTimes(2);
+    expect(emitted).toEqual([getCoinsCollectionSuccess({ coinsCollections: latestCoins })]);
+    subscription.unsubscribe();
   });
 });
